71aab690c945e683fd0b30c360d39408abae3430
winfredo <winfrednukpe2002@gmail.com>
2023-11-02 12:04:04 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Trending from "./components/Trending";
 
 function App() {
   return (
@@ -8,7 +9,10 @@ function App() {
 
   <div className="w-full xl:max-w-[1250px] mx-auto px-4" >
     <Hero />
+    <Trending />
   </div>
+
+  <div className="h-[10rem]"></div>
  </main>
 
   );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div className='flex flex-col xl:flex-row xl-h-screen justify-center pt-[5rem] w-full' >
-      <div className='flex flex-col flex-1 h-full xl-items-start justify-center text-white text-center xl-text-start space-y-8 mb-[4rem]' >
-        <p className='text-[#e87d0e] font-medium ' >Proved by prodesigner</p>
-        <h1 className='text-[2rem] font-bold w-full xl:w-[25rem]' >
-            Work that we produce for our clients
+    <div className="flex flex-col xl:flex-row xl:h-screen items-center pt-[5rem] w-full">
+      <div className="flex flex-col flex-1 h-full items-center xl:items-start justify-center text-white text-center xl:text-start space-y-8 mb-[4rem] ">
+        <p className="text-[#e87d0e] font-medium  ">Proved by prodesigner</p>
+        <h1 className="text-[2rem] font-bold w-full xl:w-[25rem] ">
+          Work that we produce for our clients
         </h1>
-      </div>
-    </div>
-  )
-}
 
-export default Hero
+        <p className="text-xs w-full xl:w-[19rem] text-gray-200 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+        <button className="text-sm w-[10rem] py-2 rounded-full bg-gradient-to-r from-[#d80027] to-[#fb9e3c]  " >
+            Get More Details
+        </button>
+      </div>
+
+      <div className="flex items-center justify-center h-[35rem] w-[10rem] overflow-hidden" >
+        <img src={require("../images/cod.jpeg")} />
+      </div>
+    
+    </div>
+  );
+}; 
+
+export default Hero;
